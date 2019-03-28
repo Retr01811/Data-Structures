cb482209b70dff0729e8671f505ea642f0b5e6d1
Ashwin <46526165+Git-Ashwin@users.noreply.github.com>
2019-03-28 15:39:23 +0530

    Branch-Ashwin

--- test.py
+++ test.py
@@ -66,12 +66,21 @@ def odd(pos):
             print(pos.ele,end=" ")
         odd(pos.left)        
         odd(pos.right)
+#swaping leaf node in binarysearchtree
 def swap(root):
-    if root:
-        swap(root.right)
-        swap(root.left)
-        (root.left,root.right)=(root.right,root.left)   
-                        
+ if root:
+  swap(root.left)
+  swap(root.right)
+  if root.right==None:
+      root.right=root.left
+      root.left=None
+  elif root.left==None:
+      root.left=root.right
+      root.right=None
+  
+  elif (root.right.right==None and root.right.left==None) or (root.left.left==None and root.left.right==None):
+          root.right,root.left=root.left,root.right
+		                   
 c=1
 a=tree(int(input("Enter the root element:")))
 while c!=0:
